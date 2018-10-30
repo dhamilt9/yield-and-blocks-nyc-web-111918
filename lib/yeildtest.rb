79753ad77c35fa4faa64a3758919da5a560ff430
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-30 21:01:55 +0000

    automatically backed up by learn

--- lib/yeildtest.rb
+++
@@ -1,9 +0,0 @@
-def yielding(num)
-  puts "the program is executing the code inside the method"
-  yield(num)
-  puts "now we are back in the method"
-end
-
-yielding(2) do |i|
-  puts "the method has yielded to the block! #{i}"
-end
